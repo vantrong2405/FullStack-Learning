7b4ec2a9a28275c25a9e82dce135b2dfa2c68e25
Van Trong <trongdn2405@gmail.com>
2024-09-10 14:44:34 +0700

    Generic type

---
+++ TypeScript/index.ts
@@ -0,0 +1,50 @@
+
+
+// function getFrist<T>(arr : T[] , n : number){
+//     return arr.slice(0, n);
+// }
+
+// getFrist<number>([1,2,3,4,5], 9);
+
+// function compare<A extends number , B extends number >(a : A , b : B){
+//     return a < b ? -1 : a > b ? 1 :0 
+// }
+
+// compare<number , number>(10,10)
+
+interface IABC {
+    a : string 
+    b : number 
+    c : string 
+}
+
+type PickAB = Pick<IABC , 'a' | 'b'>
+
+type clonePickAB<T , K extends keyof T> ={
+    [P in K] : T[P]
+} 
+
+let AB : clonePickAB<IABC , 'a' | 'b'> = {
+    a : 'a',
+    b : 1,
+}
+
+// cú pháp này lặp qua tất cả các key trong T (lúc này T là Object và K là key của T , T[K] là value của key đó)
+type WeekType<T> ={
+    [K in keyof T] : T[K]
+} 
+
+
+//cú pháp này lấy tất cả các key của object và gán cho nó là string
+type ConvertStringType1<T>={
+    [K : string] : string
+}
+type ConvertStringType2<T>={
+    [K in keyof T] : string
+}
+
+let x: ConvertStringType1<IABC> = {
+    a : 'a',
+    b : 1,// erorr vì all là string
+    c : 'c'
+}
