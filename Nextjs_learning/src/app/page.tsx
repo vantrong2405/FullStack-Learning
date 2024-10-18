89897e6b92b8b1b022777c337d8cda77026a9463
Van Trong <trongdn2405@gmail.com>
2024-10-18 22:07:35 +0700

    Sử dụng thư viện ShadcnUI

---
+++ Nextjs_learning/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+export default function Home() {
+  return (
+  <div>
+       <Button variant={'destructive'}>Click me</Button>
+       <Input className="w-56" placeholder="văn trọng"/>
+  </div>
+  );
+}
