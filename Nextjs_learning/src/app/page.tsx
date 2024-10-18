2e09d4324c007ad8757baf1fe1843d624e6618be
Van Trong <trongdn2405@gmail.com>
2024-10-18 22:26:32 +0700

    Sử dụng clsx điều kiện classname trong Nextjs

--- Nextjs_learning/src/app/page.tsx
+++ Nextjs_learning/src/app/page.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input";
+'use client'
+
+import { useState } from "react";
+import clsx from "clsx";
+import custom from './custom.module.css'
+import { Button } from "@/components/ui/button";
 export default function Home() {
+  const [expanding , setExpanding] = useState(false)
   return (
   <div>
-       <Button variant={'destructive'}>Click me</Button>
-       <Input className="w-56" placeholder="văn trọng"/>
+    <Button onClick={()=>setExpanding(!expanding)}>Click me</Button>
+    <div className={clsx('bg-yellow-500',{
+  [  custom.backgroundRed] : expanding
+  })}>
+       Hello văn trọng
+  </div>
   </div>
   );
 }
